ee3cf6e7edae27ac027e1ec569a84f9bb2a59390
Torgeir Helgevold <torgeirh@hotmail.com>
2017-03-26 13:41:22 -0400

    WIP

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,5 +1,7 @@
 import {platformBrowser} from '@angular/platform-browser';
 import {AppModuleNgFactory} from './app.module.ngfactory';
 import {Basic} from './basic';
+import {enableProdMode} from '@angular/core';
 
+enableProdMode();
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
