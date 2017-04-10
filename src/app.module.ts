d23445efe74c06301b14ca0c6834bee84948b724
Torgeir Helgevold <torgeirh@hotmail.com>
2017-04-09 20:33:35 -0400

    more fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,22 @@
+import 'rxjs/add/operator/merge';
+import 'rxjs/add/operator/repeat';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/concat';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/publishReplay';
+import 'rxjs/add/operator/bufferCount';
+import 'rxjs/add/operator/takeUntil';
+
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/forkJoin';
+import 'rxjs/add/observable/timer';
+
 import { BrowserModule }                from '@angular/platform-browser';
 import { ReactiveFormsModule }          from '@angular/forms';
 import { NgModule }                     from '@angular/core';
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { platformBrowser }    from '@angular/platform-browser';
-//import { enableProdMode } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 
 import { AppModuleNgFactory } from './app.module.ngfactory';
 
-//enableProdMode();
-
-import {addRxObjects} from './rxjs';
-
-addRxObjects();
+enableProdMode();
 
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
