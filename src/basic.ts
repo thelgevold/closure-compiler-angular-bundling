de5f32c5eff4c1fa114b9b544e759af4a81ae25b
Torgeir Helgevold <torgeirh@hotmail.com>
2017-04-14 00:08:04 -0400

    added loads

--- src/basic.ts
+++ src/basic.ts
@@ -1,6 +1,8 @@
 
 import {Component, Injectable} from '@angular/core';
 
+import * as map from 'lodash.map/index';
+
 @Component({
   selector: 'basic',
   templateUrl: './basic.ng.html',
@@ -8,5 +10,12 @@ import {Component, Injectable} from '@angular/core';
 @Injectable()
 export class Basic {
   ctxProp: string;
-  constructor() { this.ctxProp = 'Hello world!'; }
+  numbers: any[] = [];
+
+  constructor() { 
+    this.ctxProp = 'Hello world!';
+    this.numbers = map([1,2,3], (n: number) => {
+      return n * 10;
+    });
+  }
 }
